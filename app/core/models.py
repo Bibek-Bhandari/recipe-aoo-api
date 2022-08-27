8cc2933a5be43c7bd1ae299815607f5a3cfef334
bibekbhandari <bibekbhandari2014@gmail.com>
2022-08-27 16:53:50 +0200

    Remove flake8 test errors

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,13 @@
 """
 Database models.
 """
-from django.db import models # noqa
+from django.db import models  # noqa
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -14,7 +15,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError('User must have an email address.')
+            raise ValueError("User must have an email address.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -30,13 +31,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects= UserManager()
+    objects = UserManager()
 
-    USERNAME_FIELD = 'email' 
+    USERNAME_FIELD = "email"
