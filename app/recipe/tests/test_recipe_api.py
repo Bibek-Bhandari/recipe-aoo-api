2ca6111c26a90cda974e8a293561a26cd38f8a04
bibekbhandari <bibekbhandari2014@gmail.com>
2022-09-30 16:02:33 +0200

    Implement recipe listing  api

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -17,7 +17,7 @@ from core.models import Recipe
 from recipe.serializers import RecipeSerializer
 
 
-RECIPIES_URL = reverse('recipe:recipe-list')
+RECIPES_URL = reverse('recipe:recipe-list')
 
 def create_recipe(user, **params):
     """Create and return a sample recipe."""
