6f9b37fd8dd9caaf3479894c912540a9ef83bd5d
bibekbhandari <bibekbhandari2014@gmail.com>
2022-09-30 16:55:49 +0200

    Implement create recipe api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, User
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -17,6 +17,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """Retrive recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
@@ -24,3 +25,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return serializers.RecipeSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
